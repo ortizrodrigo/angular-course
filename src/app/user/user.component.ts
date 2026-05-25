8a60bb71c6039dc280d8700e9192b429bbb6d5b2
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-05-24 23:54:24 -0700

    Ch. 2 Angular Essentials - Components, Templates, Services and More (57% complete)

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+// type User = { id: string; avatar: string; name: string };
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
