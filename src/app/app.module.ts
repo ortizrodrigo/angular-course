b879fafce7a97fb3f7ca40b1665ee08607ed74a2
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-05-26 00:15:51 -0700

    Ch. 3: Angular Essentials - Working with Modules (100% complete)

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { App } from './app';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [App, HeaderComponent, UserComponent],
+  bootstrap: [App],
+  imports: [BrowserModule, SharedModule, TasksModule],
+})
+export class AppModule {}
