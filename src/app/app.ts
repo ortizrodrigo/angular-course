76976563da17bdbfd03c445e45095a9512c59494
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-05-25 21:58:15 -0700

    Ch. 2 Angular Essentials - Components, Templates, Services and More (71% complete)

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class App {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-// type User = { id: string; avatar: string; name: string };
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +9,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
